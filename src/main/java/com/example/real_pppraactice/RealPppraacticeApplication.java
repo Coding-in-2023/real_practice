4973013510c271ab62d2f6ae7bd335f96fb4d226
leeshinbi <tlsql6831@naver.com>
2023-08-15 15:56:26 +0900

    first commit

---
+++ src/main/java/com/example/real_pppraactice/RealPppraacticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.real_pppraactice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealPppraacticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealPppraacticeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/real_pppraactice/first/hello.java
@@ -0,0 +1,4 @@
+package com.example.real_pppraactice.first;
+
+public class hello {
+}
